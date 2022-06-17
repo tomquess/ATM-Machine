b80772a241bc5ae55f5ab6d90cc3fc3cb9352c14
Matimateokol <58779750+Matimateokol@users.noreply.github.com>
2022-06-18 00:50:59 +0200

    Add disabled test 2

--- ATM/src/test/java/OptionMenuTest.java
+++ ATM/src/test/java/OptionMenuTest.java
@@ -1,9 +1,15 @@
 package src.test.java;
 
+import junit.framework.Assert;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import src.main.java.Account;
 import src.main.java.OptionMenu;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.InputMismatchException;
 
 public class OptionMenuTest {
@@ -20,5 +26,33 @@ public class OptionMenuTest {
 
         //Assert
         //assertEquals();
+    }
+
+    @Disabled("bugged test. TODO: fix it!")
+    @Test
+    void getAccountType_() {
+
+        //Arrange
+        InputStream stdin = System.in;
+        System.setIn(new ByteArrayInputStream("4".getBytes()));
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(byteArrayOutputStream);
+        PrintStream stdout = System.out;
+        System.setOut(ps);
+
+        OptionMenu optionMenu = new OptionMenu();
+        Account acc = new Account(1, 4321, 1200.0, 600.0);
+        optionMenu.getAccountType(acc);
+
+        System.setIn(stdin);
+        System.setOut(stdout);
+
+        String outputText = byteArrayOutputStream.toString();
+        String key = "output";
+        String output = outputText.substring(outputText.indexOf(key) + key.length()).trim();
+
+
+        Assert.assertEquals(output, "\nInvalid Choice.");
     }
 }
