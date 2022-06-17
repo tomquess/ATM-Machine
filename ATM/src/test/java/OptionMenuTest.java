bcac1dec9d8d722537deaed4ede0c66380f3eb42
Matimateokol <58779750+Matimateokol@users.noreply.github.com>
2022-06-18 00:04:46 +0200

    Add disabled test

--- ATM/src/test/java/OptionMenuTest.java
+++ ATM/src/test/java/OptionMenuTest.java
@@ -1,5 +1,24 @@
 package src.test.java;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import src.main.java.OptionMenu;
+
+import java.util.InputMismatchException;
+
 public class OptionMenuTest {
 
+    @Disabled("Not implemented yet! I will do it tomorrow...")
+    @Test
+    void getLogin_GivenInvalidCharacters_ThrowsInputMismatchException(){
+
+        //Arrange
+        OptionMenu optionMenu = new OptionMenu();
+
+        //Act
+        //var result = optionMenu.getLogin();
+
+        //Assert
+        //assertEquals();
+    }
 }
